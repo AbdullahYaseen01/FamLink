54370262815ca29331f7ebdd44c297afd89f8e80
ggps2002 <155824827+ggps2002@users.noreply.github.com>
2025-09-20 12:14:32 +0530

    remove the bg image from register for event and add a single colored bg

--- frontend/src/NewComponents/Events.jsx
+++ frontend/src/NewComponents/Events.jsx
@@ -6,11 +6,10 @@ function Events() {
   return (
     <div
       className="h-screen w-full flex justify-center items-center 
-      bg-[url('https://images.unsplash.com/photo-1630465651806-09682f8364b5?q=80&w=1066&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] 
-      bg-cover bg-center relative overflow-hidden"
+    bg-[#F6F3EE] relative overflow-hidden"
     >
       {/* Dark overlay */}
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      {/* <div className="absolute inset-0 bg-black bg-opacity-50"></div> */}
 
       {/* Logo link pinned top-left */}
       <NavLink
@@ -19,14 +18,14 @@ function Events() {
         className="absolute top-4 left-4 flex gap-1 items-center z-10"
       >
         <img src="/logo3.png" alt="logo" className="w-6 h-6 sm:w-8 sm:h-8" />
-        <p className="font-bold text-lg sm:text-xl Livvic-Bold text-white">
+        <p className="font-bold text-lg sm:text-xl Livvic-Bold">
           Famlink
         </p>
       </NavLink>
 
       {/* Card */}
-      <div className="relative bg-white rounded-2xl shadow-2xl p-8 w-[90%] max-w-md text-center z-10">
-        <h1 className="text-3xl font-bold text-gray-900 mb-4">
+      <div className="relative bg-white rounded-2xl shadow-soft p-8 w-[90%] max-w-md text-center z-10">
+        <h1 className="text-3xl Livvic-SemiBold text-gray-900 mb-4">
           Register for Event
         </h1>
         <p className="text-gray-600 mb-6">
