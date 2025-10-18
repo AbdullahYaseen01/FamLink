6d1ba0af137386f102fb900fb1dc2f1735a62d4d
ggps2002 <155824827+ggps2002@users.noreply.github.com>
2025-10-18 16:47:55 +0530

    Add banner toggling to session storage, remove unwanted files and folders for faster js load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,23 +8,11 @@ import {
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import PageLayout from "./pageLayout";
-import Home from "./Components/Home/home";
-import Families from "./Components/Families/Families";
-import JobSeekers from "./Components/Job-Seekers/jobSeekers";
 import IndividualProfile from "./Components/Profiles/profiles";
 import NannyShare from "./NewComponents/NannyShare/NannyShare";
 import Services from "./Components/Services/services";
-import ListYourBusiness from "./Components/ListYouryourBusiness/listYourBusiness";
 import JoinNow from "./Components/JoinNow/joinNow";
-import Tutor from "./Components/subComponents/Hire/NannyShareNo/AdditionalStep.jsx/tutor";
-import Swim from "./Components/subComponents/Hire/NannyShareNo/AdditionalStep.jsx/swim";
-import SpecialCaregiver from "./Components/subComponents/Hire/NannyShareNo/AdditionalStep.jsx/specializedCare";
-import HouseManager from "./Components/subComponents/Hire/NannyShareNo/AdditionalStep.jsx/houseMang";
-import Music from "./Components/subComponents/Hire/NannyShareNo/AdditionalStep.jsx/music";
-import SportCoach from "./Components/subComponents/Hire/NannyShareNo/AdditionalStep.jsx/sportCoach";
-import HireForm from "./Components/JoinNow/Hire";
 import Job from "./Components/JoinNow/Job";
-import CommunitySign from "./Components/JoinNow/CommunitySign";
 import ForgetPass from "./Components/Forget-Password/forgetPass";
 import TutorJob from "./Components/subComponents/Job/MultipleStep/tutor";
 import SwimJob from "./Components/subComponents/Job/MultipleStep/swim";
@@ -41,15 +29,12 @@ import Setting from "./Components/LoginAsFamily/setting";
 import Message from "./Components/LoginAsFamily/Message";
 import Booking from "./Components/LoginAsFamily/Booking/booking";
 import Favorites from "./Components/LoginAsFamily/favorite";
-import TipsAndArticles from "./Components/LoginAsFamily/community";
 import Community from "./Components/LoginAsFamily/tipsAndArticles";
-import DetailsCommArt from "./Components/LoginAsFamily/detailsCommArt";
 import TrustsAndSafety from "./Components/LoginAsFamily/trustAndSafety";
 import HowItWorks from "./Components/LoginAsFamily/howItWorks";
 import Desktop1 from "./Components/LoginAsFamily/CommDesktop/desktop1";
 import Desktop2 from "./Components/LoginAsFamily/CommDesktop/desktop2";
 import Desktop3 from "./Components/LoginAsFamily/CommDesktop/desktop3";
-import TransHist from "./Components/LoginAsFamily/transHistory";
 
 import Nanny from "./Components/LoginAsNanny/nanny";
 import UserProfileNanny from "./Components/LoginAsNanny/userProfile";
@@ -61,21 +46,17 @@ import BookingNanny from "./Components/LoginAsNanny/booking";
 import FavoritesNanny from "./Components/LoginAsNanny/favourite";
 import TipsAndArticlesNanny from "./Components/LoginAsNanny/tipsAndArticles";
 import DetailsCommArtNanny from "./Components/LoginAsNanny/detailsCommArt";
-import TransHistNanny from "./Components/LoginAsNanny/transHist";
 import Application from "./Components/LoginAsNanny/application";
 import WithdrawEarning from "./Components/LoginAsNanny/withdrawEarning";
 import Login from "./Components/Login/login";
-import ComingSoon from "./Components/subComponents/comingSoon";
 import NannyShareComponent from "./Components/LoginAsFamily/nannyShare";
 import NewHireForm from "./Components/JoinNow/NewHire";
 import PostAJob from "./Components/LoginAsFamily/PostAJob/postAJob";
 import JobListing from "./Components/LoginAsFamily/JobListing/job-listing";
 import JobListingView from "./Components/LoginAsFamily/JobListing/job-listing-view";
 import { PostANannyShare } from "./Components/LoginAsFamily/PostANannyShare/postANannyShare";
-import { NannyShareView } from "./Components/LoginAsFamily/PostANannyShare/detailsNannyShare";
 import { useNotifications } from "./Config/useNotification";
 import Pricing from "./Components/Price/pricing";
-import CommunityPost from "./Components/subComponents/community";
 import NewHome from "./NewComponents/Home/Home";
 import TermsAndConditions from "./Components/Authority/Terms&Condition";
 import Caregivers from "./NewComponents/Caregivers/Caregivers";
@@ -113,37 +94,27 @@ function App() {
           <>
             <Route path="/" element={<NannyShare />} />
             <Route path="/login" element={<Login />} />
-            {/* <Route path="/pricing" element={<Pricing />} /> */}
             <Route
               path="/terms-and-conditions"
               element={<TermsAndConditions />}
             />
             <Route path="/forgetPass" element={<ForgetPass />} />
-            <Route path="/forFamilies" element={<Families />} />
             <Route path="/events" element={<Events />} />
             <Route path="/jobSeekers" element={<Caregivers />} />
             <Route path="/families" element={<NewHome />} />
             <Route path="/business" element={<Business />} />
             <Route path="/services" element={<Services />} />
-            <Route path="/yourBusiness" element={<ListYourBusiness />} />
             <Route path="/joinNow" element={<JoinNow />} />
             <Route path="/hire" element={<NewHireForm />} />
             <Route path="/job" element={<Job />} />
-            {/* <Route path="/communitySign" element={<CommunitySign />} /> */}
-            {/* <Route path='/tutor' element={<Tutor />} /> */}
             <Route path="/tutorJob" element={<TutorJob />} />
-            {/* <Route path='/swim' element={<Swim />} /> */}
             <Route path="/swimJob" element={<SwimJob />} />
-            {/* <Route path='/specialCaregiver' element={<SpecialCaregiver />} /> */}
             <Route
               path="/specialCaregiverJob"
               element={<SpecialCaregiverJob />}
             />
-            {/* <Route path='/houseManager' element={<HouseManager />} /> */}
             <Route path="/houseManagerJob" element={<HouseManagerJob />} />
-            {/* <Route path='/music' element={<Music />} /> */}
             <Route path="/musicJob" element={<MusicJob />} />
-            {/* <Route path='/sportCoach' element={<SportCoach />} /> */}
             <Route path="/sportCoachJob" element={<SportCoachJob />} />
           </>
         )}
@@ -194,11 +165,6 @@ function App() {
             <Route path="jobListing" element={<JobListing />} />
             <Route path="jobListingView/:id" element={<JobListingView />} />
             <Route path="trustsAndSafety" element={<TrustsAndSafety />} />
-            {/* <Route path='transHistory' element={<TransHist />} /> */}
-            <Route path="desktop1/:id" element={<Desktop1 />} />
-            <Route path="desktop2/:id/:topicId" element={<Desktop2 />} />
-            <Route path="desktop3/:id/:postId" element={<Desktop3 />} />
-            <Route path="details/:id" element={<DetailsCommArt />} />
           </Route>
         )}
 
@@ -222,14 +188,6 @@ function App() {
             <Route path="howItWorks" element={<HowItWorks />} />
             <Route path="trustsAndSafety" element={<TrustsAndSafety />} />
             <Route path="details/:id" element={<DetailsCommArtNanny />} />
-            {/* <Route
-              path='community'
-              element={<TipsAndArticles nanny={true} />}
-            /> */}
-            {/* <Route
-              path='community'
-              element={<ComingSoon />}
-            /> */}
             <Route path="desktop1/:id" element={<Desktop1 nanny={true} />} />
             <Route
               path="desktop2/:id/:topicId"
@@ -239,7 +197,6 @@ function App() {
               path="desktop3/:id/:postId"
               element={<Desktop3 nanny={true} />}
             />
-            {/* <Route path='transHistory' element={<TransHistNanny />} /> */}
             <Route path="application" element={<Application />} />
             <Route path="withdrawEarning" element={<WithdrawEarning />} />
           </Route>
