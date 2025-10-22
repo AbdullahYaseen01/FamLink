1a812d86feeff8006aee256e6399b7cf42d5b608
ggps2002 <155824827+ggps2002@users.noreply.github.com>
2025-10-22 11:58:44 +0530

    remove unused pngs and add purge css vite plugin to remove all unused css during build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from "rollup-plugin-visualizer";
+import purgecss from "vite-plugin-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
+  purgecss({
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}", // all source files where class names might appear
+    ],
+    safelist: [/^ant-/, /^slick-/, /^swiper-/, /^react-/, /^emoji-/], // ⚠️ important for Ant Design and other libs
+  }),
   visualizer({
     open: true,
     filename: "dist/bundle-report.html",
@@ -14,7 +22,7 @@ export default defineConfig({
   ],
   build: {
     sourcemap: true, // 👈 add this line
-    minify: 'esbuild', 
+    minify: 'esbuild',
   },
   server: {
     host: '0.0.0.0',      // ← allow external access
