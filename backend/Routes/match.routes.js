5010a9412b91788a3d6e173e4158063911f52337
ggps2002 <155824827+ggps2002@users.noreply.github.com>
2026-05-27 00:01:28 +0530

    fix linux file sensitivity issue

--- backend/Routes/match.routes.js
+++ backend/Routes/match.routes.js
@@ -2,12 +2,11 @@ import express from "express";
 import {
   requestMatch,
   getNearbyMatches,
-} from "../Controllers/match.controller.js";
+} from "../controllers/match.controller.js";
 import { authMiddleware } from "../Services/utils/middlewareAuth.js";
 
 const router = express.Router();
 
 router.post("/request", authMiddleware, requestMatch);
-router.get("/nearby/:userId", getNearbyMatches);
 
 export default router;
